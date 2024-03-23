ffb582237007d050e5b6205f73c9a7803c15adb5
KingJrn <emmajnr503@protonmail.com>
2024-03-23 12:38:58 +0100

    Added new page

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -4,8 +4,8 @@
   
 </template>
 <script>
-  import Sidebar from '../components/sidebar.vue';
-  import Settings from '../components/Admin/Settings.vue';
+  // import Sidebar from '../'
+  // import Settings from './components/Admin/Settings.vue';
   export default {
     name: "Admin",
     component: {
